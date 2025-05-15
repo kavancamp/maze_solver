fba19098d8209787fc2848635fb3fd78a167deb3
kavancamp <keenahvancampenhout@gmail.com>
2025-05-14 22:21:16 -0500

    updated window class

--- window.py
+++ window.py
@@ -1,17 +1,15 @@
 import tkinter as tk
 from tkinter import Tk, BOTH, Canvas
 import time
+from point import Point
+from line import Line
 
 class Window:
-    def __init__(self, width=700, height=600):
+    def __init__(self, width, height):
         self.root = Tk()
-        self.width = width
-        self.height = height 
-        self.root.title(title="Maze Solver")
+        self.root.title("Maze Solver")
         self.is_running = False
-        self.root.geometry(f"{width}x{height}")
         self.root.protocol("WM_DELETE_WINDOW", self.close) 
-        
         self.canvas = Canvas(self.root, width=width, height=height, bg='white')
         self.canvas.pack(fill=BOTH, expand=1)
         
@@ -24,11 +22,11 @@ class Window:
 
         button = tk.Button(self.root, text="Close Program", command=self.close)
         button.pack()
-
         
     def redraw(self):
         self.root.update_idletasks()
-        self.root.update()    
+        self.root.update()
+
     
     def wait_for_close(self):
         self.is_running = True   
@@ -42,3 +40,6 @@ class Window:
     def close(self):
         self.is_running = False
         #self.root.destroy() 
+        
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)    
