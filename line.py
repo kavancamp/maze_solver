5dbb99986b28e71b0d084fe585f5be0a2ff877b9
kavancamp <keenahvancampenhout@gmail.com>
2025-05-14 22:21:33 -0500

    added draw method

--- line.py
+++ line.py
@@ -1,9 +1,18 @@
 from point import Point
+from tkinter import Canvas
 
 class Line:
     def __init__(self, start, end):
         self.start = start  # instance of Point
         self.end = end      # instance of Point
-
+        
+    def draw(self, canvas, fill_color="black"): 
+        canvas.create_line(
+            self.start.x, self.start.y,
+            self.end.x, self.end.y,
+            fill=fill_color,
+            width=2
+        )
     def __repr__(self):
         return f"Line(start={self.start}, end={self.end})"
+    
