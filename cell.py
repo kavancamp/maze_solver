18409e487ba2c2ebb6f14e53335e9e75b8ac3c0e
kavancamp <keenahvancampenhout@gmail.com>
2025-05-16 14:47:19 -0500

    updated cell class

--- cell.py
+++ cell.py
@@ -2,10 +2,10 @@ from line import Line
 from point import Point
 
 class Cell:
-    def __init__(self, x1, x2, y1, y2, win=None):
+    def __init__(self, x1, y1, x2, y2, win=None):
         self._x1 = x1
-        self._x2 = x2
         self._y1 = y1
+        self._x2 = x2
         self._y2 = y2
         self._win = win
         
@@ -14,43 +14,34 @@ class Cell:
         self.has_top_wall = True
         self.has_bottom_wall = True
         
+        self.visited = False # ← Track maze traversal
+        
     def draw(self):
         if self._win is None:
             return
-        # Draw top wall
-        if self.has_top_wall:
-            top = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
-            self._win.draw_line(top)
 
-        # Draw bottom wall
-        if self.has_bottom_wall:
-            bottom = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
-            self._win.draw_line(bottom)
+        def maybe_draw(condition, start, end):
+            color = "black" if condition else "white"
+            line = Line(start, end)
+            self._win.draw_line(line, color)
 
-        # Draw left wall
-        if self.has_left_wall:
-            left = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
-            self._win.draw_line(left)
+        # Draw each wall depending on existence
+        maybe_draw(self.has_top_wall,    Point(self._x1, self._y1), Point(self._x2, self._y1))
+        maybe_draw(self.has_bottom_wall, Point(self._x1, self._y2), Point(self._x2, self._y2))
+        maybe_draw(self.has_left_wall,   Point(self._x1, self._y1), Point(self._x1, self._y2))
+        maybe_draw(self.has_right_wall,  Point(self._x2, self._y1), Point(self._x2, self._y2))
 
-        # Draw right wall
-        if self.has_right_wall:
-            right = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
-            self._win.draw_line(right)
-            
     def draw_move(self, to_cell, undo=False):
-        #change color to show backtracking
-        fill_color = "gray" if undo else "red" 
+        if self._win is None:
+            return
 
-        # Calculate centers of current cell and destination cell
-        half_length = abs(self._x2 - self._x1) // 2
-        x_center = half_length + self._x1
-        y_center = half_length + self._y1
+        color = "gray" if undo else "red"
 
-        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
-        x_center2 = half_length2 + to_cell._x1
-        y_center2 = half_length2 + to_cell._y1
-        
-        # Create a line from this cell's center to the other
-        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
-        self._win.draw_line(line, fill_color)
-                
+        # Compute center points
+        x1 = (self._x1 + self._x2) // 2
+        y1 = (self._y1 + self._y2) // 2
+        x2 = (to_cell._x1 + to_cell._x2) // 2
+        y2 = (to_cell._y1 + to_cell._y2) // 2
+
+        line = Line(Point(x1, y1), Point(x2, y2))
+        self._win.draw_line(line, color)
